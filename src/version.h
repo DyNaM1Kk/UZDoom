cc304467f46583a717b9e676abdc902a6f704ccf
DyNaM1Kk <42520902+DyNaM1Kk@users.noreply.github.com>
2026-03-02 04:17:49 +0400

    Added updater
    Pulls latest release from GitHub, unzips the archive or pulls the AppImage in case of Linux, extracts to temp directory and replaces each original files with the new versions.

--- src/version.h
+++ src/version.h
@@ -39,9 +39,9 @@ const char *GetVersionString();
 #define RC_PRODUCTVERSION 4,9999,9999,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
-#define VER_MAJOR 5
-#define VER_MINOR 0
-#define VER_REVISION 0
+#define VER_MAJOR 4
+#define VER_MINOR 13
+#define VER_REVISION 2
 
 // This should always refer to the UZDoom version a derived port is based on and not reflect the derived port's version number!
 #define ENG_MAJOR 5
